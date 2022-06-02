49579a7d7aa01be49ef36d71b0b7c2958713a56a
Jon Prosser <jon.prosser@capgemini.com>
2022-06-02 22:07:00 +0100

    added middleware and asyncHandler

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,28 @@
-const registerUser = (req, res) => {
-    res.send('Register Route')
-}
+const asyncHandler = require('express-async-handler')
 
-const loginUser = (req, res) => {
+// register a new user
+// @route /api/users
+// access Public
+const registerUser = asyncHandler( async (req, res) => {
+
+   const {name, email, password} =  req.body
+
+   //validation
+   if(!name || !email || !password){
+       res.status(400)
+       throw new Error('Please include all fields')
+   }
+
+    res.send('Register Route')
+})
+
+
+// login a new user
+// @route /api/users/login
+// access Public
+const loginUser = asyncHandler(  async (req, res) => {
     res.send('Login Route')
-}
+})
 
 module.exports = {
     registerUser,
